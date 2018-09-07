8e0f90a721ae848f6f1112af1a5ced10279c9c30
Jerome Louis <j.louis@oodrive.com>
2018-09-07 13:52:54 +0200

    Add devServerConfig property do dev-server

--- packages/dev-server/src/schemes.ts
+++ packages/dev-server/src/schemes.ts
@@ -1,7 +1,7 @@
 module.exports = [
   {
     originalSchemaPath: '@angular-devkit/build-angular/src/dev-server/schema.json',
-    schemaExtensionPaths: [`${__dirname}/generic/schema.ext.json`],
+    schemaExtensionPaths: [`${__dirname}/generic/schema.ext.json`, `${__dirname}/schema.ext.json`],
     newSchemaPath: `${__dirname}/generic/schema.json`
   }
 ];
